6ecc2b710ea63084f6a8768fa9070f69ac1bb222
王权 <17612172372@163.com>
2026-04-03 19:13:09 +0800

    qiankun-vue配置修改

--- qiankun-vue/vue.config.js
+++ qiankun-vue/vue.config.js
@@ -1,23 +1,23 @@
-const packageName = 'vueApp';
+const packageName = "vueApp";
 
 module.exports = {
   // 生产环境打包到二级目录 /vue，并让静态资源引用以 /vue/ 为前缀
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue/' : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/vue/" : "/",
   // 生产环境输出目录
-  outputDir: 'dist/vue',
+  outputDir: "dist",
 
   devServer: {
     port: 10000,
     // 支持跨域
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*",
     },
   },
 
   configureWebpack: {
     output: {
       library: packageName,
-      libraryTarget: 'umd',
+      libraryTarget: "umd",
       jsonpFunction: `webpackJsonp_${packageName}`,
     },
   },
